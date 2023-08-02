816e81f7531454c68508bc1b1f976e5dcf6e1d91
Adam Cooper <arc@hilltop-cottage.info>
2023-08-02 22:53:32 +0100

    WIP. Now succeeds in completing the Z2M interview. Part way through adding a LED.

--- main.py
+++ main.py
@@ -26,17 +26,30 @@ if f1.command == znp.SYS_RESET_IND:
 # ------------ Configuration stored to NV memory on device
 success = znp.zb_write_configuration(S,
                                      b'\x87',  # ZCD_NV_LOGICAL_TYPE
-                                     b'\x02')  # end device
+                                     b'\x02',  # end device
+                                     print_msg=True)
 if success:
     success = znp.zb_write_configuration(S,
                                          b'\x83',  # ZCD_NV_PAN_ID
-                                         b'\xff\xff')  # "dont care" - tolerate whatever the coordinator has. Expected to be OK if only one PAN.
+                                         b'\xff\xff',  # "dont care" - tolerate whatever the coordinator has. Expected to be OK if only one PAN.
+                                         print_msg=True)
 # could set the channel mask here - use ZCD_NV_CHANLIST - but presume that the firmware defaults will be suitable (same firmware as coordinator)
 # For tinkering, set ZCD_NV_STARTUP_OPTION to cause clear network state on restart. Otherwise the device will attempt to resume its network, which might be bollocks.
 if success:
     success = znp.zb_write_configuration(S,
                                          b'\x03',  # ZCD_NV_STARTUP_OPTION
-                                         b'\x02')  # clear NV memory. Also possible to clear config settings.
+                                         b'\x02',  # clear NV memory. Also possible to clear config settings.
+                                         print_msg=True)
+
+# >>> this modifies the ZCD_NV_POLL_RATE, which controls the interval with which the device contacts the coordinator with IEEE 802.15.4 "Data Request" packets
+# (see Wireshark sniffer). The compiled default is 1000ms. THe documentation wrongly states this is config_id 0x24 and has byte length. It is actually
+# config_id = 0x35 and is a 4 byte value (in ms) expressed in little-endian form
+if success:
+    success = znp.zb_write_configuration(S,
+                                         b'\x35',  # ZCD_NV_POLL_RATE
+                                         b'\x98\x3A\x00\x00',  # 15,000ms, as PTVO, little-endian
+                                         print_msg=True)
+
 if not success:
     raise Exception("FAILED to write device basic configuration")
 
@@ -45,13 +58,23 @@ print("Resetting to clear network state.")
 znp.command_no_data(S, znp.ZB_SYSTEM_RESET)  # ignore response frame (but this DOES wait for it)
 print("Reset complete.")
 
-# ---------- Register the device and define the clusters. There is an alternative command in the Simple API
+# ---------- Register the device and define the clusters. There is an alternative command in the Simple API. IDs here are big-endian
 znp.af_register(S,
                 endpoint=b'\x01',  # A single endpoint is being used, 1
                 app_prof_id=b'\x01\x04',  # Home Automation Profile (prescribed) = 260 decimal
                 app_device_id=b'\x00\x00',  # Device ID also in the HA Profile spec - this is the On/Off switch Id
                 app_dev_ver=b'\x01',  # I think the device version is not prescribed
-                in_cluster_ids=(b'\x00\x00',),  # only Basic Cluster, which is device info such as name, manufacturer etc TODO in, out, or both for Basic?
+                in_cluster_ids=(  # this is a tuple
+                    b'\x00\x00',  # Basic Cluster, which is device info such as name, manufacturer etc
+                    b'\x00\x06'  # On/Off LED - the messaging is IN to the device, which is therefore a "server"
+                    # switch config would go here (PTVO switches have this)
+                ),
+                out_cluster_ids=(  # this is a tuple
+                    # PTVO GPIO LED also has an out cluster id 0x0006, which reports its state periodically (interval set in PTVO app), and this would
+                    # presumably cover the case where there is an on/off switch on the device itself as well as remote control of the LED
+                    b'\x00\x06',
+                    #  b'\x00\x12',  # On/Off switch is in output cluster list and so is a "client". This is actually a "Multistate". Plain on/off would be 0x0006
+                ),
                 print_msg=True
                 )
 
@@ -61,17 +84,22 @@ znp.af_register(S,
 # - ZDO_STARTUP_FROM_APP, which takes a parameter and returns a RSP with a network state byte
 input("MAKE SURE Zigbee2MQTT is accepting join requests and then hit any key. (Otherwise you get status=2 INVALID_PARAMETER from ZDO_STATE_CHANGE_IND)")
 fr = znp.send_and_await_response(S,
-                                 znp.append_fcs(b'\xfe\x01\x25\x40\x00'),  # ZDO_STARTUP_FROM_APP with 0 delay
+                                 b'\x01\x25\x40\x00',  # ZDO_STARTUP_FROM_APP with 0 delay
                                  print_msg=True)  # this should cause a response 0x6540 with network state code + multiple ZDO_STATE_CHANGE_IND
 device_ready = False
-while not device_ready:  # TODO put a loop count and exit
+state_2_count = 0
+while not device_ready:
     if fr.command == b'\x65\x40':
         print("Startup response: network status code = {}".format(fr.data[0].to_bytes(length=1, byteorder="big")))
     elif fr.command == b'\x45\xC0':  # ZDO_STATE_CHANGE_IND
         state = fr.data[0]
-        if state != 2:
+        if state == 2:
+            state_2_count += 1
+        else:
             print(f"State = {state}")
         device_ready = state == 6  # DEV_END_DEVICE
+        if state_2_count > 20:
+            raise Exception("Excessive state=2 from ZDO_STATE_CHANGE_IND. Coordinator probably not accepting joins or off-line.")
     else:
         print("Recd cmd: {}".format(fr.command.hex()))
     if not device_ready:
@@ -80,16 +108,72 @@ print("Joined coordinator network.")
 
 # ------------- THIS IS WHERE Z2M Starts its interview, issuing multiple AF_INCOMING_MSG
 running = True
+# set up for reporting events at 10s intervals
+from time import time
+last_time_toggle = int(time() // 10) % 2
+# device state variables
+device_output_onoff = False  # LED off
 while running:
     # check for incoming messages which correspond to the Z2M "interview"
     if S.inWaiting():
         f = znp.ZnpFrameBody(S)
+        print("RX body:", f)
+        # first trap informative stuff (at least as far as we are treating things)
+        if f.command == b'\x44\x80':  # AF_DATA_CONFIRM
+            print("AF_DATA_CONFIRM for TransId={} on Endpoint={} had Status={}".format(f.data[2], f.data[1], f.data[0]))
+            continue
+
         in_msg = znp.AfIncomingMessage(f)
         if in_msg.is_af_incoming_message:
-            print(in_msg.zcl_raw.hex(" ", 1))
-            # TODO should probably check cluster ID AND ZCL command id and respond here. the incoming message may ask for one attribute or several,
-            # and the response should bundle accordingly
+            print("Incoming ZCL for endpoint {}, cluster id = {} is: {}".format(in_msg.dst_endpoint[0],
+                                                                                in_msg.cluster_id.hex(),
+                                                                                in_msg.zcl_raw.hex(" ")))
+            if in_msg.zcl.zcl_command == b'\x00':  # Read Attributes
+                if in_msg.cluster_id == b'\x00\x00':
+                    cluster_provider = znp.BasicClusterAttributeParts(model_identifier="ZNP-Test")
+                else:
+                    cluster_provider = None
+
+                if cluster_provider is None:
+                    print("Unsupported cluster {}; cannot respond".format(in_msg.cluster_id.hex()))
+                data = znp.ZclFrameReadAttributesResponse(response_to=in_msg.zcl,
+                                                          cluster_provider=znp.BasicClusterAttributeParts(model_identifier="ZNP-Test")).zcl_message()
+                # AF_DATA_REQUEST 0x2401
+                print("Sending response...")
+                out_msg = (10 + len(data)).to_bytes(length=1, byteorder="big") + b'\x24\x01' + \
+                    in_msg.src_addr + in_msg.src_endpoint + in_msg.dst_endpoint + in_msg.cluster_id + in_msg.transaction_seq_no + b'\x00' + b'\x10' +\
+                    len(data).to_bytes(length=1, byteorder="big") + data
+                rsp_success = znp.send_and_check_success(S, out_msg, b'\x64\x01', print_msg=True)
+                print("AF_DATA_REQUEST_RSP success?", rsp_success)  # ALSO likely to be AF_DATA_CONFIRM for request, in addition to AF_DATA_REQUEST response
         else:
-            print("Not AF_INCOMING_MSG")
+            print("Unexpected message")
+            print("RX body:", f)
+
+            # *** this is how PTVO LED reports its state, which it does on report period (not when state externally changed)
+            # ZigBee Network Layer Data, Dst: 0x0000, Src: 0xb85a
+            # ZigBee Application Support Layer Data, Dst Endpt: 1, Src Endpt: 1
+            #     Frame Control Field: Data (0x00)
+            #     Destination Endpoint: 1
+            #     Cluster: On/Off (0x0006)
+            #     Profile: Home Automation (0x0104)
+            #     Source Endpoint: 1
+            #     Counter: 8
+            # ZigBee Cluster Library Frame, Command: Report Attributes, Seq: 2
+            #     Frame Control Field: Profile-wide (0x08)
+            #     Sequence Number: 2
+            #     Command: Report Attributes (0x0a)
+            #     Attribute Field
+            #         Attribute: OnOff (0x0000)
+            #         Data Type: Boolean (0x10)
+            #         On/off Control: Off (0x00)
+
+    # output event?
+    time_toggle = int(time() // 10) % 2
+    if time_toggle != last_time_toggle:
+        last_time_toggle = time_toggle
+        print("do event...")
+        device_output_onoff = not device_output_onoff
 
 S.close()
+
+
--- znp.py
+++ znp.py
@@ -13,14 +13,20 @@ class BasicClusterAttributeParts:
     def get_part(self, attribute_id):
         """
 
-        :param attribute_id:
+        :param attribute_id: big-endian attribute id
         :type attribute_id: bytes
         :return:
         """
-        part = None
+
+        # we need the little-endian form for response messages, while working with big-endian for user-facing and Python API parameters
+        attribute_id_le = attribute_id[::-1]
+
+        # Default = use a status of 0x86, which means UNSUPPORTED_ATTRIBUTE and include no value
+        part = attribute_id_le + b'\x86'
+
         # Commented out elifs are those which are requested by Z2M but for which I will return a "not supported" (for now)
-        if attribute_id == b'\x00\x00':  # ZCL Version - what should this be for HA 1.2?????
-            pass
+        if attribute_id == b'\x00\x00':  # ZCL Version
+            part = attribute_id_le + b'\x00' + b'\x30' + b'\x08'  # - what should this be for HA 1.2????? Used 8 for now as latest ZCL doc (which is Z3!)
         # elif attribute_id == b'\x00\x01':  # ApplicationVersion
         #     pass
         # elif attribute_id == b'\x00\x02':  # StackVersion
@@ -28,17 +34,15 @@ class BasicClusterAttributeParts:
         # elif attribute_id == b'\x00\x03':  # HWVersion
         #     pass
         elif attribute_id == b'\x00\x04':
-            part = attribute_id + b'\x00' + zcl_string(self.manufacturer_name)
+            part = attribute_id_le + b'\x00' + zcl_string(self.manufacturer_name)
         elif attribute_id == b'\x00\x05':
-            part = attribute_id + b'\x00' + zcl_string(self.model_identifier)
+            part = attribute_id_le + b'\x00' + zcl_string(self.model_identifier)
         # elif attribute_id == b'\x00\x06':  # DateCode
         #     pass
         elif attribute_id == b'\x00\x07':  # PowerSource. This is mandatory!
-            part = attribute_id + b'\x00' + b'\x30' + b'\x00'  # last byte means "unknown"
+            part = attribute_id_le + b'\x00' + b'\x30' + b'\x03'  # last byte 0x00 means "unknown", 0x03 means "battery"
         # elif attribute_id == b'\x40\x00':  # SWBuildID
         #     pass
-        else:  # use a status of 0x86, which means UNSUPPORTED_ATTRIBUTE and include no value
-            part = attribute_id + b'\x86'
 
         return part
 
@@ -52,7 +56,59 @@ def zcl_string(s):
     return b'\x42' + len(s).to_bytes(length=1, byteorder="big") + s.encode(encoding="ascii")
 
 
-class ZclFrame:
+class ZclFrameCommand:
+    """
+    Base class for ZCL messages received
+    """
+    def __init__(self, data):
+        """
+        :param data: "data" component from ZNP message
+        :type data: bytes
+        :
+        """
+        self.frame_control = data[0].to_bytes(length=1, byteorder="big")
+        self.trans_seq_no = data[1].to_bytes(length=1, byteorder="big")  # NB as byte as this will be needed for replies
+        self.zcl_command = data[2].to_bytes(length=1, byteorder="big")
+
+
+class ZclFrameResponse:
+    """
+    Base class for a response to a ZNP message
+    """
+    def __init__(self, frame_control, trans_seq_no, zcl_command):
+        """
+        Params are all 1 byte length bytes objects
+        :param frame_control:
+        :type frame_control: bytes
+        :param trans_seq_no:
+        :param zcl_command:
+        """
+        self.frame_control = frame_control
+        self.trans_seq_no = trans_seq_no
+        self.zcl_command = zcl_command
+        self.variables = []  # populate this in sub-class. this is the list of items for the message body
+
+    def zcl_header(self):
+        return self.frame_control + self.trans_seq_no + self.zcl_command
+
+    def zcl_message(self):
+        """
+        returns header + body for ZCL response as a bytes object
+        :return: bytes
+        """
+        return self.zcl_header() + b''.join(self.variables)
+
+
+def zcl_fcf_flip(fcf):
+    """
+    FLips direction of a frame-control field
+    :param fcf:
+    :return:
+    """
+    return (fcf[0] ^ 0x08).to_bytes(1, "big")  # direction is bit 3
+
+
+class ZclFrameReadAttributes(ZclFrameCommand):
     """
     Parses an AF-level data payload to ZCL components. TODO fix this to not just be for request frames.
     """
@@ -62,13 +118,27 @@ class ZclFrame:
         :param data: "data" component of e.g. AF_INCOMING_MSG
         :type data: bytes
         """
+        super(ZclFrameReadAttributes, self).__init__(data)
 
-        self.frame_control = data[0].to_bytes(length=1, byteorder="big")
-        self.trans_seq_no = data[1].to_bytes(length=1, byteorder="big")  # NB as byte as this will be needed for replies
-        self.zcl_command = data[2].to_bytes(length=1, byteorder="big")
         self.attribute_ids = []  # converted to big-endian from little-endian in the raw data
         for i in range(3, len(data), 2):
             self.attribute_ids.append(data[i+1: i-1: -1])
+
+
+class ZclFrameReadAttributesResponse(ZclFrameResponse):
+    """
+    Creates a response to read-attributes command
+    """
+    def __init__(self, response_to, cluster_provider):
+        """
+
+        :param response_to: ZCL command frame object (subclass of ZclFrameCommand)
+        :type response_to: ZclFrameReadAttributes
+        :param cluster_provider: object with get_part(attribute_id) method, which will generate the data parts for the response
+        """
+        super(ZclFrameReadAttributesResponse, self).__init__(zcl_fcf_flip(response_to.frame_control), response_to.trans_seq_no, b'\x01')
+
+        self.variables = [cluster_provider.get_part(a) for a in response_to.attribute_ids]
 
 
 class AfIncomingMessage:
@@ -89,9 +159,9 @@ class AfIncomingMessage:
 
         # the command id says the frame is an AF_INCOMING_MESSAGE, so parse it. Unfortunately, there appears to be no documentation on the structure
         # other than what I can infer from how Z-tool shows the message. For example, I DONT KNOW WHETHER THE 16bit PARTS ARE BIG-ENDIAN or not
-        self.group_id = f.data[0:2]
-        self.cluster_id = f.data[2:4]
-        self.src_addr = f.data[4:6]
+        self.group_id = f.data[0:2][::-1]  # this and next two come in as little-endian but Python API and UI convention is big-endian
+        self.cluster_id = f.data[2:4][::-1]
+        self.src_addr = f.data[4:6][::-1]
         self.src_endpoint = f.data[6].to_bytes(length=1, byteorder="big")
         self.dst_endpoint = f.data[7].to_bytes(length=1, byteorder="big")
         self.was_broadcast = f.data[8].to_bytes(length=1, byteorder="big")
@@ -101,7 +171,7 @@ class AfIncomingMessage:
         self.transaction_seq_no = f.data[15].to_bytes(length=1, byteorder="big")
         self.data_len = f.data[16]  # somewhat redundant as a public property
         self.zcl_raw = f.data[17:17+self.data_len]  # oddly, the raw frame data has 3 extra bytes after those indicated by data_len, which don't show in sniffer
-        self.zcl = ZclFrame(self.zcl_raw)
+        self.zcl = ZclFrameReadAttributes(self.zcl_raw)
 
 
 class ZnpFrameBody:  # i.e. the ZNP frame as sent over UART but without the SOF byte and the FCS byte. Length becomes implicit in len(self.data)
@@ -143,7 +213,7 @@ class ZnpFrameBody:  # i.e. the ZNP frame as sent over UART but without the SOF 
             print(f"BAD! Computed XOR8 = {xor8}. Compare to frame FCS = {fcs}")
 
     def __str__(self):
-        return self.command.hex() + ": " + self.data.hex(sep=' ', bytes_per_sep=1)
+        return "Cmd: " + self.command.hex() + " Body: " + self.data.hex(sep=' ')
 
 
 # ZNP message command ids
@@ -152,31 +222,35 @@ ZB_WRITE_CONFIGURATION_RSP = b'\x66\x05'
 ZB_SYSTEM_RESET = b'\x46\x09'
 
 
-def append_fcs(msg):  # note that bytes objects are immutable
+def calc_append_fcs(msg):  # note that bytes objects are immutable
     """
     Append XOR8 checksum to message
-    :param msg: message, including the SOF
+    :param msg: message, EXcluding the SOF
     :type msg: bytes
     :return: full framed message from SOF to FCS inclusive
     """
     xor8 = 0
-    for b in msg[1:]:
+    for b in msg:
         xor8 = xor8 ^ b
     return msg + xor8.to_bytes(length=1, byteorder="big")
 
 
-def send_and_await_response(s, msg, print_msg=False):
+def send_and_await_response(s, msg, prepend_sof=True, append_fcs=True, print_msg=False):
     """
 
     :param s:
     :param msg:
+    :param prepend_sof: whether to add 0xfe before the passed message = the Start of Frame byte
+    :param append_fcs: whetner to compute the XOR8 checksum and append to passed message before sending
     :param print_msg:
     :return:
     """
 
+    framed_message = (b'\xfe' if prepend_sof else b'') + (calc_append_fcs(msg) if append_fcs else msg)
+
     if print_msg:
-        print("TX:", msg.hex(sep=" ", bytes_per_sep=1))
-    s.write(msg)
+        print("TX:", framed_message.hex(sep=" "))
+    s.write(framed_message)
 
     f = ZnpFrameBody(s)
     if print_msg:
@@ -185,17 +259,19 @@ def send_and_await_response(s, msg, print_msg=False):
     return f
 
 
-def send_and_check_success(s, msg, response_command_id, print_msg=False):
+def send_and_check_success(s, msg, response_command_id, prepend_sof=True, append_fcs=True,  print_msg=False):
     """
 
     :param s:
     :param msg:
-    :param response_command_id: 2 byte command id of response message expected for sent msg
+    :param response_command_id: 2 byte command id of response message expected for sent msg:
+    :param prepend_sof: whether to add 0xfe before the passed message = the Start of Frame byte
+    :param append_fcs: whetner to compute the XOR8 checksum and append to passed message before sending
     :param print_msg:
     :return:
     """
     # wait for response and return boolean
-    f = send_and_await_response(s, msg, print_msg)
+    f = send_and_await_response(s, msg, prepend_sof, append_fcs,  print_msg)
     return f.command == response_command_id and f.data == b'\x00'  # 1 data byte = 0x00 for "success"
 
 
@@ -213,14 +289,14 @@ def zb_write_configuration(s, config_id, value, print_msg=False):
     :return:
     """
     data_len = 2 + len(value)  # config-id + value-len + value
-    msg = b'\xfe' + data_len.to_bytes(length=1, byteorder='big') + b'\x26\x05' + config_id + len(value).to_bytes(length=1, byteorder="big") + value
-
-    msg = append_fcs(msg)
+    msg = data_len.to_bytes(length=1, byteorder='big') + b'\x26\x05' + config_id + len(value).to_bytes(length=1, byteorder="big") + value
 
     return send_and_check_success(s,
                                   msg,
                                   b'\x66\x05',  # ZB_WRITE_CONFIGURATION_RSP
-                                  print_msg)
+                                  prepend_sof=True,
+                                  append_fcs=True,
+                                  print_msg=print_msg)
 
 
 def af_register(s,
@@ -234,11 +310,11 @@ def af_register(s,
                 print_msg=False
                 ):
     """
-
+    NB: the two-byte parameters are passed in big-endian form (and are converted internally to this function). Includes cluster ids
     :param s:
     :param endpoint: single byte for endpoint no/id
-    :param app_prof_id: profile id - generally  Home Automation Profile, a prescribed id from Zigbee specs
-    :param app_device_id: device id - generally a prescribed id from the profile
+    :param app_prof_id: 2 byte profile id - generally  Home Automation Profile, a prescribed id from Zigbee specs
+    :param app_device_id: 2 byte device id - generally a prescribed id from the profile
     :param app_dev_ver: I think the device version is not prescribed
     :param latency_req: see ZNP docs. I have not looked into whether values other than the default are useful
     :param in_cluster_ids: list/tuple of cluster ids, each being a 2 byte id. may be empty
@@ -246,16 +322,21 @@ def af_register(s,
     :param print_msg:
     :return:
     """
+    # little-endian needed for msg
+    le_in_cluster_ids = [cid[::-1] for cid in in_cluster_ids]
+    le_out_cluster_ids = [cid[::-1] for cid in out_cluster_ids]
     data_len = 9 + 2 * len(in_cluster_ids) + 2 * len(out_cluster_ids)
-    msg = b'\xfe' + data_len.to_bytes(length=1, byteorder='big') + b'\x24\x00' + \
-          endpoint + app_prof_id + app_device_id + app_dev_ver + latency_req + \
-          len(in_cluster_ids).to_bytes(length=1, byteorder="big") + b''.join(in_cluster_ids) + \
-          len(out_cluster_ids).to_bytes(length=1, byteorder="big") + b''.join(out_cluster_ids)
+    msg = data_len.to_bytes(length=1, byteorder='big') + b'\x24\x00' + \
+        endpoint + app_prof_id[::-1] + app_device_id[::-1] + app_dev_ver + latency_req + \
+        len(in_cluster_ids).to_bytes(length=1, byteorder="big") + b''.join(le_in_cluster_ids) + \
+        len(out_cluster_ids).to_bytes(length=1, byteorder="big") + b''.join(le_out_cluster_ids)
 
     return send_and_check_success(s,
-                                  append_fcs(msg),
+                                  msg,
                                   b'\x64\x00',
-                                  print_msg)
+                                  prepend_sof=True,
+                                  append_fcs=True,
+                                  print_msg=print_msg)
 
 
 def command_no_data(s, command_id):
@@ -266,8 +347,8 @@ def command_no_data(s, command_id):
     :type command_id: bytes
     :return:
     """
-    msg = b'\xfe\x00' + command_id
-    s.write(append_fcs(msg))
+    msg = b'\x00' + command_id
+    s.write(b'\xfe' + calc_append_fcs(msg))  # add SOF and FCS
 
     # wait for response and return it
     return ZnpFrameBody(s)
