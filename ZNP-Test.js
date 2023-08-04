3647bdc9d7092a0ed17e57b31a9c818eed306740
Adam Cooper <arc@hilltop-cottage.info>
2023-08-04 15:59:05 +0100

    This converter works with the Py code. It is derivative of PTVO single LED and is probably not minimal.

---
+++ ZNP-Test.js
@@ -0,0 +1,58 @@
+const zigbeeHerdsmanConverters = require('zigbee-herdsman-converters');
+const zigbeeHerdsmanUtils = require('zigbee-herdsman-converters/lib/utils');
+
+
+const exposes = zigbeeHerdsmanConverters.exposes;
+const ea = exposes.access;
+const e = exposes.presets;
+const fz = zigbeeHerdsmanConverters.fromZigbeeConverters;
+const tz = zigbeeHerdsmanConverters.toZigbeeConverters;
+
+const ptvo_switch = zigbeeHerdsmanConverters.findByDevice({modelID: 'ptvo.switch'});
+fz.legacy = ptvo_switch.meta.tuyaThermostatPreset;
+fz.ptvo_on_off = {
+  cluster: 'genOnOff',
+  type: ['attributeReport', 'readResponse'],
+  convert: (model, msg, publish, options, meta) => {
+      if (msg.data.hasOwnProperty('onOff')) {
+          const channel = msg.endpoint.ID;
+          const endpointName = `l${channel}`;
+          const binaryEndpoint = model.meta && model.meta.binaryEndpoints && model.meta.binaryEndpoints[endpointName];
+          const prefix = (binaryEndpoint) ? model.meta.binaryEndpoints[endpointName] : 'state';
+          const property = `${prefix}_${endpointName}`;
+	  if (binaryEndpoint) {
+            return {[property]: msg.data['onOff'] === 1};
+          }
+          return {[property]: msg.data['onOff'] === 1 ? 'ON' : 'OFF'};
+      }
+  },
+};
+
+
+
+const device = {
+    zigbeeModel: ['ZNP-Test'],
+    model: 'ZNP-Test',
+    vendor: 'ARC12',
+    description: '[ZNP testing WIP]',
+    fromZigbee: [fz.ignore_basic_report, fz.ptvo_on_off,],
+    toZigbee: [tz.ptvo_switch_trigger, tz.on_off,],
+    exposes: [e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2')
+],
+    meta: {
+        multiEndpoint: true,
+        
+    },
+    endpoint: (device) => {
+        return {
+            l1: 1, l2: 2
+        };
+    },
+    configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+      await endpoint.read('genBasic', ['modelId', 'swBuildId', 'powerSource']);
+    },
+
+};
+
+module.exports = device;
